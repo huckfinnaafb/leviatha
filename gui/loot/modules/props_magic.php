77ef7b5b624b97f4654707f11984cb5df2e140af
Samuel Ferrell <huckfinnaafb@gmail.com>
2011-03-29 11:49:33 -0400

    COMPLETE: magic property translations

--- gui/loot/modules/props_magic.php
+++ gui/loot/modules/props_magic.php
@@ -5,13 +5,12 @@
             try {
                 if (isset($this->db_item["prop_magic"]) && (!empty($this->db_item["prop_magic"]))) {
                     foreach($this->db_item["prop_magic"] as $row) {
-                            echo "<li>{$row["translation"]}</li>\n";
+                        echo "<li>{$row["translation"]}</li>\n";
                     }
                 } else {
                     throw new Exception("No magic properties found.");
                 }
-            }
-            catch (Exception $e) {
+            } catch (Exception $e) {
                 echo "<div class='module mod-notify mod-warning'>" . $e->getMessage(), "</div>\n";
             }
         ?>
--- gui/loot/modules/props_set.php
+++ gui/loot/modules/props_set.php
@@ -1,39 +1,18 @@
 <div class="module mod-info">    
     <h1 class="h-info">Individual Set Bonuses</h1>
-    <table class="table-info">
-        <tbody>
-            <?php
-                try {
-                    if (isset($this->db_item["prop_set"]) && (!empty($this->db_item["prop_set"]))) {
-                        foreach($this->db_item["prop_set"] as $row) {
-                            echo "<tr class='set'>";
-                                echo "<td>{$row['translation']}</td>";
-                                if (isset($row["param"])) 
-                                    echo "<td>{$row['param']}</td>";
-                                else
-                                    echo "<td></td>";
-                                
-                                if (isset($row["min"]) && isset($row["max"])) {
-                                    if ($row["min"] == $row["max"]) {
-                                        echo "<td>{$row["min"]}</td>";
-                                    } else {
-                                        echo "<td>{$row["min"]} - {$row["max"]}</td>";
-                                    }
-                                } else {
-                                    echo "<td></td>";
-                                }
-                                
-                                echo "<td>( " . ($row["req_equip"] + 1) . " Equipped )</td>";
-                                
-                            echo "</tr>";
-                        }
-                    } else {
-                        throw new Exception("No set bonuses found.");
+    <ul class="list-iteminfo set">
+        <?php
+            try {
+                if (isset($this->db_item["prop_set"]) && (!empty($this->db_item["prop_set"]))) {
+                    foreach($this->db_item["prop_set"] as $row) {
+                        echo "<li>{$row["translation"]} (" . ($row["req_equip"] + 1) . " Equipped)</li>\n";
                     }
-                } catch (Exception $e) {
-                    echo "<p class='module mod-notify mod-warning'>" . $e->getMessage(), "</p>\n";
+                } else {
+                    throw new Exception("No set bonuses found.");
                 }
-            ?>
-        </tbody>
-    </table>
+            } catch (Exception $e) {
+                echo "<div class='module mod-notify mod-warning'>" . $e->getMessage(), "</div>\n";
+            }
+        ?>
+    </ul>
 </div>
