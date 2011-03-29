4168b0ae38a45fd8195cac9a093f1e803a786dfd
Samuel Ferrell <huckfinnaafb@gmail.com>
2011-03-29 11:52:31 -0400

    family properties gui table -> list

--- gui/loot/modules/props_set_family.php
+++ gui/loot/modules/props_set_family.php
@@ -1,43 +1,18 @@
 <div class="module mod-info">
     <h1 class="h-info">Family Set Bonuses</h1>
-    <table class="table-info family">
-        <tbody>
-            <?php
-                try {
-                    if (isset($this->db_family["props"]) && (!empty($this->db_family["props"]))) {
-                        foreach($this->db_family["props"] as $row) {
-                            echo "<tr>";
-                                echo "<td>{$row['translation']}</td>";
-                                if (isset($row["parameter"])) 
-                                    echo "<td>{$row['parameter']}</td>";
-                                else
-                                    echo "<td></td>";
-                                
-                                if (isset($row["min"]) && isset($row["max"])) {
-                                    if ($row["min"] == $row["max"]) {
-                                        echo "<td>{$row["min"]}</td>";
-                                    } else {
-                                        echo "<td>{$row["min"]} - {$row["max"]}</td>";
-                                    }
-                                } else {
-                                    echo "<td></td>";
-                                }
-                                
-                                if (count($this->db_family["members"]) == $row["req_equip"]) {
-                                    echo "<td>( Full Set Bonus )</td>";
-                                } else {
-                                    echo "<td>( " . ($row["req_equip"]) . " Equipped )</td>";
-                                }
-                                
-                            echo "</tr>";
-                        }
-                    } else {
-                        throw new Exception("No set bonuses found.");
+    <ul class="list-iteminfo family">
+        <?php
+            try {
+                if (isset($this->db_family["props"]) && (!empty($this->db_family["props"]))) {
+                    foreach($this->db_family["props"] as $row) {
+                        echo "<li>{$row["translation"]} ({$row["req_equip"]} Equipped)</li>\n";
                     }
-                } catch (Exception $e) {
-                    echo "<p class='module mod-notify mod-warning'>" . $e->getMessage(), "</p>\n";
+                } else {
+                    throw new Exception("No set bonuses found.");
                 }
-            ?>
-        </tbody>
-    </table>
+            } catch (Exception $e) {
+                echo "<p class='module mod-notify mod-warning'>" . $e->getMessage(), "</p>\n";
+            }
+        ?>
+    </ul>
 </div>
