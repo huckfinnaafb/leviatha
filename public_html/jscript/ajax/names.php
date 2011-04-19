1fe6568c6b1f44d0f72226db7afb684c06135dce
Samuel Ferrell <huckfinnaafb@gmail.com>
2011-04-19 12:32:23 -0400

    and those ajax files too

---
+++ public_html/jscript/ajax/names.php
@@ -0,0 +1,14 @@
+<?php
+define ('__SITE_PATH', "/home/huckfinnaafb/www.leviatha.org/");
+// PHP Fat Free Framework (http://fatfree.sourceforge.net/)
+require_once (__SITE_PATH . "/library/F3/F3/F3.php");
+F3::set('AUTOLOAD',
+    __SITE_PATH . "/application/controllers/|" .
+    __SITE_PATH . "/library/F3/autoload/"
+);
+F3::config(__SITE_PATH . "/application/config/config.cfg");
+$leviatha = array();
+foreach(F3::sql("SELECT name FROM loot") as $row) {
+    array_push($leviatha, $row["name"]);
+}
+echo json_encode($leviatha);
