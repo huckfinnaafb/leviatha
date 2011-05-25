19dd2f0e2abb98b06de8da4bdc3cd787fc17969d
Samuel Ferrell <huckfinnaafb@gmail.com>
2011-05-25 17:24:37 -0400

    oh and these fiels too

---
+++ application/models/SearchModel.php
@@ -0,0 +1,38 @@
+<?php
+/**
+    Author: Samuel Ferrell
+    Purpose: Search the Database and return results
+**/
+class SearchModel extends RootModel {
+    
+    public $query = array(
+        "items" => "
+            SELECT name, urlname, level, levelreq, rarity, COALESCE (class, division) AS relationship
+            FROM loot
+            WHERE urlname LIKE :term
+        "
+    );
+    
+    /**
+        Loot Search - Leave pagination up to Javascript/JQuery
+            @return mysql resource
+            @param $query string
+    **/
+    public function items($term) {
+        
+        // URL Friendly Item Name
+        $term = F3::slug($term);
+        
+        // Strip illegal characters
+        $term = str_replace(array("'"), "", $term);
+        
+        // Replace spaces
+        $term = str_replace(" ", "-", $term);
+        
+        // Wildcards
+        $term = "%" . $term . "%";
+        
+        // Query
+        return F3::sqlBind($this->query['items'], array('term' => $term)) ?: false;
+    }
+}
