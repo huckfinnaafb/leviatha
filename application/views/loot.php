ca24d56c3ffbcd2eac5d180b33517b0b56a92c37
Samuel Ferrell <huckfinnaafb@gmail.com>
2011-06-18 00:03:07 -0400

    simplified variant/similar/siblings template conditions

--- application/views/loot.php
+++ application/views/loot.php
@@ -80,7 +80,7 @@
         <?php
         
             // Family Members
-            if (isset($this->siblings) && ($this->siblings)) { ?>
+            if ($this->siblings) { ?>
                 <div class="mod mod-side style-shadow">
                     <h1 class="h-side h-box style-gradient"><?php echo $this->item->family; ?> Family</h1>
                     <?php
@@ -99,7 +99,7 @@
             <?php }
         
             // Variants
-            if (isset($this->variants) && ($this->variants)) { ?>
+            if ($this->variants) { ?>
                 <div class="mod mod-side style-shadow">
                     <h1 class="h-side h-box style-gradient">Variants</h1>
                     
@@ -118,7 +118,7 @@
             <?php }
             
             // Similar
-            if (isset($this->similar) && ($this->similar)) { ?>
+            if ($this->similar) { ?>
                 <div class="mod mod-side style-shadow">
                     <h1 class="h-side h-box style-gradient">Similar</h1>
                     
