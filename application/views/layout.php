06bac39972695093671bdcfa384223f2d376566d
Samuel Ferrell <huckfinnaafb@gmail.com>
2011-06-20 23:50:31 -0400

    hide google analytics on dev

--- application/views/layout.php
+++ application/views/layout.php
@@ -62,17 +62,20 @@
             }
         ?>
         
-        <!-- Google Analytics -->
-        <script type="text/javascript">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-21869873-1']);
-            _gaq.push(['_trackPageview']);
+        <?php if (F3::get('RELEASE')) { ?>
+            <!-- Google Analytics -->
+            <script type="text/javascript">
+                var _gaq = _gaq || [];
+                _gaq.push(['_setAccount', 'UA-21869873-1']);
+                _gaq.push(['_trackPageview']);
 
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-        </script>
+                (function() {
+                    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                })();
+            
+            </script>
+        <?php } ?>
     </body>
 </html>
