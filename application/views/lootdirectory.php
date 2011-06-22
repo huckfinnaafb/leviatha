5f79305bf77153a6be89c9006d49377a2e5533fc
Samuel Ferrell <huckfinnaafb@gmail.com>
2011-06-22 00:35:50 -0400

    loot directory stuff

--- application/views/lootdirectory.php
+++ application/views/lootdirectory.php
@@ -1,25 +1,21 @@
 <div class="line">
-    <?php if (file_exists(__SITE_PATH . "/application/views/lootdirectorycache.html")) { 
-        include (__SITE_PATH . "/application/views/lootdirectorycache.html"); 
-    } else { ?>
-        <?php foreach($this->kingdoms as $k=>$kingdom) { ?>
-            <div class="unit size1of4">
-                <div class="mod">
-                    <h1 class="h-side style-gradient"><?php echo $kingdom; ?></h1>
-                    <?php foreach ($this->types as $type) { 
-                        if ($type['kingdom'] == $k) { ?>
-                            <ul class="list-directory">
-                                <li><h2><?php echo ucwords($type['type']); ?></h2></li>
-                                <?php foreach($this->items as $item) { 
-                                    if ($item['type'] == $type['code']) { ?>
-                                        <li><a href="/loot/<?php echo $item['urlname']; ?>"><?php echo $item['name']; ?></a></li>
-                                    <?php }
-                                } ?>
-                            </ul>
-                        <?php } 
-                    } ?>
-                </div>
+    <?php foreach($this->kingdoms as $k=>$kingdom) { ?>
+        <div class="unit size1of4">
+            <div class="mod">
+                <h1 class="h-side style-gradient"><?php echo $kingdom; ?></h1>
+                <?php foreach ($this->types as $type) { 
+                    if ($type['kingdom'] == $k) { ?>
+                        <ul class="list-directory">
+                            <li><h2><?php echo ucwords($type['type']); ?></h2></li>
+                            <?php foreach($this->items as $item) { 
+                                if ($item['type'] == $type['code']) { ?>
+                                    <li><a href="/loot/<?php echo $item['urlname']; ?>"><?php echo $item['name']; ?></a></li>
+                                <?php }
+                            } ?>
+                        </ul>
+                    <?php } 
+                } ?>
             </div>
-        <?php } ?>
+        </div>
     <?php } ?>
 </div>
